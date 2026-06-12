52f9a159b27370a81db69b1abd403c78afd4cd57
redpymeia360-cloud <redpymeia360@gmail.com>
2026-06-12 16:41:51 -0400

    Delete config.example.js

--- config.example.js
+++
@@ -1,8 +0,0 @@
-window.TDL_CONFIG = {
-  supabaseUrl: 'https://TU-PROYECTO.supabase.co',
-  supabaseAnonKey: 'TU-ANON-PUBLIC-KEY',
-  storageBucket: 'crm-files',
-  mode: 'supabase',
-  whatsapp: '56920185428',
-  instagramReel: 'https://www.instagram.com/reel/DZL333LMT3h/?igsh=dGtwMDNya3JsOGFs'
-};
